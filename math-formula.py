25e4b585db7c9278c163e1d3697653f1b84d8d79
Kento <ken.f@cris.ac.th>
2022-05-11 21:00:33 +0700

    An update version now include Surface Area (cylinder_sa, cone_sa)

--- math-formula.py
+++ math-formula.py
@@ -13,6 +13,14 @@ def cone(r,h):
 	a = ((r ** 2) * pi  * h)/3
 	print(a)
 
+def cylinder_sa(r,h):
+    a = (2 * h * r *pi)+(pi * (r ** 2))
+    print(a)
+
+def cone_sa(r,l):
+    a = (pi * r * l)+(pi * (r ** 2))
+    print(a)
+
 if cmd == "sphere":
 	r = int(input("Enter the radius:\n"))
 	sphere(r)
@@ -24,5 +32,13 @@ elif cmd == "cone":
 	r = int(input("Enter the radius:\n"))
 	h = int(input("Enter the height:\n"))
 	cone(r,h)
+elif cmd == "cylinder_sa":
+    r = int(input("Enter the radius:\n"))
+    h = int(input("Enter the height:\n"))
+    cylinder(r,h)
+elif cmd == "cone_sa":
+    r = int(input("Enter the radius:\n"))
+    l = int(input("Enter the slant height:\n"))
+    cone_sa(r,l)
 else:
 	print(f"There is no Formula Name {cmd}")
