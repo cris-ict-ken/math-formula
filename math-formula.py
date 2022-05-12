329a990550746d6bc3d6af5753ce18dd69b4c853
Kento <ken.f@cris.ac.th>
2022-05-12 19:39:39 +0700

    New update

--- math-formula.py
+++ math-formula.py
@@ -1,4 +1,10 @@
-cmd = input("What Formula you wanna use?\n")
+cmd_type = input('''What type of formula you wanna use?
+ 1 volume
+ 2 surface area\n''')
+cmd = input('''What formula you wanna use? 
+ 1 cylinder
+ 2 cone
+ 3 sphere\n''')
 pi = 3.14
 
 def sphere(r):
@@ -21,24 +27,33 @@ def cone_sa(r,l):
     a = (pi * r * l)+(pi * (r ** 2))
     print(a)
 
-if cmd == "sphere":
-	r = int(input("Enter the radius:\n"))
-	sphere(r)
-elif cmd == "cylinder":
-	r = int(input("Enter the radius:\n"))
-	h = int(input("Enter the height:\n"))
-	cylinder(r,h)
-elif cmd == "cone":
-	r = int(input("Enter the radius:\n"))
-	h = int(input("Enter the height:\n"))
-	cone(r,h)
-elif cmd == "cylinder_sa":
-    r = int(input("Enter the radius:\n"))
-    h = int(input("Enter the height:\n"))
-    cylinder(r,h)
-elif cmd == "cone_sa":
-    r = int(input("Enter the radius:\n"))
-    l = int(input("Enter the slant height:\n"))
-    cone_sa(r,l)
+def sphere_sa(r):
+    a = ((r ** 2 ) * pi * 4)
+    print(a)
+
+if cmd_type == "volume":
+    if cmd == "sphere":
+	    r = float(input("Enter the radius:\n"))
+	    sphere(r)
+    elif cmd == "cylinder":
+	    r = float(input("Enter the radius:\n"))
+	    h = float(input("Enter the height:\n"))
+	    cylinder(r,h)
+    elif cmd == "cone":
+	    r = float(input("Enter the radius:\n"))
+	    h = float(input("Enter the height:\n"))
+	    cone(r,h)
+elif cmd_type == "surface area":
+    if cmd == "cylinder":
+        r = float(input("Enter the radius:\n"))
+        h = float(input("Enter the height:\n"))
+        cylinder_sa(r,h)
+    elif cmd == "cone":
+        r = float(input("Enter the radius:\n"))
+        l = float(input("Enter the slant height:\n"))
+        cone_sa(r,l)
+    elif cmd == "sphere":
+        r = float(input("Enter the radius\n"))
+        sphere_sa(r)
 else:
-	print(f"There is no Formula Name {cmd}")
+	print("An error had occur pls make sure it all lowercase or spell correctly, Thanks - Monke")
